dcebb7284b5356117be6fa0691fd502c1924ddaa
arjoel <lavelapuerca12345@hotmail.com>
2022-09-20 21:16:38 -0300

    Update Ejercicio 2.6.c

--- Ejercicio 2.6.c
+++ Ejercicio 2.6.c
@@ -2,28 +2,32 @@
 
 /*El precio para un vuelo es de $8800 en clase turista y se aplica un incremento del 30% en primera clase.
 Se ingresan la cantidad de pasajes vendidos de clase turista y de primera clase.
-Obtener la recaudación total del vuelo. */
+Obtener la recaudaciÃ³n total del vuelo. */
 
 
-int main(){
+int main()
+{
 
-int clase_turista = 8800;
-int primera_clase;
-int clase_turista_vendidos= 20;
-int primera_clase_vendidos= 10;
-int recaudacion_total;
-primera_clase = (8800 * 30)/ 100;
-recaudacion_total = primera_clase+clase_turista;
+    int clase_turista = 8800;
+    int primera_clase_adicion;
+    int clase_turista_vendidos= 20;
+    int primera_clase_vendidos= 10;
+    int recaudacion_total;
+    int primera_clase;
 
-
-printf ("\nSe vendieron %d vuelos en la clase turista", clase_turista_vendidos);
-printf ("\nSe vendieron %d vuelos en la primera clase", primera_clase_vendidos);
-printf ("\nLa recaudacion total fue de %d",recaudacion_total);
-
+    primera_clase_adicion = (clase_turista * 30)/ 100;
+    primera_clase= primera_clase_adicion+clase_turista;
+    recaudacion_total = primera_clase+clase_turista;
 
 
 
-return 0;
+    printf ("\nSe vendieron %d vuelos en la clase turista", clase_turista_vendidos);
+    printf ("\nSe vendieron %d vuelos en la primera clase", primera_clase_vendidos);
+    printf ("\nLa recaudacion total fue de %d",recaudacion_total);
+
+
+
+
+    return 0;
 
 }
-
