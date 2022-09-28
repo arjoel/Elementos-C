d3a7b6e22ae1f3ad80b6c1da4483bff1aead8290
arjoel <lavelapuerca12345@hotmail.com>
2022-09-28 18:10:53 -0300

    Create Ejercicio 4.3.c

---
+++ Ejercicio 4.3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+//Confeccionar un programa que pueda determinar de 3 números enteros que se ingresan si alguno de ellos
+//es igual a la suma de los otros dos.
+
+int main()
+{
+
+    int a,b,c = 0;
+    int suma_igual_a,suma_igual_b,suma_igual_c= 0;
+    printf("ingrese un numero: ");
+    scanf("%d",&a);
+    printf("ingrese un numero: ");
+    scanf("%d",&b);
+    printf("ingrese un numero: ");
+    scanf("%d",&c);
+
+
+    if (a= b+c)
+    {
+        printf("\n%d es igual a la suma de %d y %d", a, b, c);
+
+    }
+    if (b= a+c)
+    {
+        printf("\n%d es igual a la suma de %d y %d", b, a, c);
+
+    }
+    if (c= a+b)
+    {
+        printf("\n%d es igual a la suma de %d y %d", c, a, b);
+
+    }
+
+
+    return 0;
+}
