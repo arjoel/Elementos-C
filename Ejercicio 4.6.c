d19932897b79e7a7e8d616428362464d744ba57a
arjoel <lavelapuerca12345@hotmail.com>
2022-10-01 20:24:32 -0300

    Create Ejercicio 4.6.c

---
+++ Ejercicio 4.6.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/*Confeccionar un programa que permita ingresar 4 números enteros, de a uno por vez y determine el menor
+valor y su número de orden. Informe los valores ingresados e identifique al menor con mensaje aclaratorio.*/
+
+int main()
+
+
+{
+    int num1,num2,num3,num4, menor = 0;
+    printf("ingrese el primer numero : ");
+    scanf("%d",&num1);
+    printf("ingrese el segundo numero : ");
+    scanf("%d",&num2);
+    printf("ingrese el tercer numero : ");
+    scanf("%d",&num3);
+    printf("ingrese el cuarto numero : ");
+    scanf("%d",&num4);
+
+    if ( num1 < num2  && num1 < num3 && num1 < num4 )
+    {
+        menor = num1;
+    }
+    if ( num2 < num1 && num2 < num3 && num2 < num4)
+    {
+        menor = num2;
+    }
+
+    if (num3 < num2 && num3 < num2 && num3 < num1)
+    {
+        menor = num3;
+    }
+    if (num4 < num1 && num4 < num2 && num4 < num3)
+    {
+        menor = num4;
+    }
+    printf("\nlos numeros fueron ingresados en el siguiente orden: %d %d %d %d", num1, num2, num3, num4);
+
+    printf("\nel menor numero ingresado fue: %d", menor);
+
+
+    return 0;
+}
+
